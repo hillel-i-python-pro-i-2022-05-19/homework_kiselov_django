470289780cfe89feab66fe5f2f388b6d855f3cbe
ykyselv <ivanes96@gmail.com>
2022-09-06 17:24:24 +0300

    H

--- apps/phone_book/forms.py
+++ apps/phone_book/forms.py
@@ -1,9 +1,59 @@
 from django import forms
 from phonenumber_field.formfields import PhoneNumberField
+from apps.phone_book.models import Contact, Detail,Tag
+from django.forms.models import inlineformset_factory,BaseInlineFormSet
+from .contact_validator import contact_validator
+from django.core.exceptions import ValidationError
 
 
-class CreateForm(forms.Form):
-    contact_name = forms.CharField(
-        label="contact_name",
-    )
-    phone_value = PhoneNumberField(label="contact_value")
+
+class DetailForm(forms.ModelForm):
+    class Meta:
+        model =  Detail
+        fields = (
+            "contact_type",
+            "contact_value",
+            "contact"
+        )
+
+    def clean(self):
+        cleaned_data = super(DetailForm, self).clean()
+        if contact_validator(cleaned_data.get("contact_type"), cleaned_data.get("contact_value")) == False:
+            raise ValidationError(f'{cleaned_data.get("contact_type")} введён некорректно')
+
+        return cleaned_data
+
+
+
+
+class CreateForm(forms.ModelForm):
+    details = forms.ModelMultipleChoiceField(Detail.objects.filter(contact=None))
+    class Meta:
+        model = Contact
+        fields = (
+            "contact_name",
+            "birthday",
+            "tags",
+            "contact_avatar",
+            "creator",
+            "details"
+        )
+
+
+class UpdateForm(forms.ModelForm):
+    details = forms.ModelMultipleChoiceField(Detail.objects.all())
+    class Meta:
+        model = Contact
+        fields = (
+            "contact_name",
+            "birthday",
+            "tags",
+            "contact_avatar",
+            "creator",
+            "details"
+        )
+
+
+
+
+
--- apps/phone_book/views.py
+++ apps/phone_book/views.py
@@ -1,18 +1,20 @@
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, ListView, DeleteView, UpdateView
-from .models import Contact
+from django.views.generic import TemplateView, ListView, DeleteView, UpdateView, CreateView
+from .models import Contact,Tag, Detail
 from django.views import View
-from .forms import CreateForm
+from .forms import CreateForm, DetailForm, UpdateForm
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpRequest
+from .query_to_dict import querydict_to_dict
 
 class MainpageView(TemplateView):
     template_name = "phone_book/base.html"
 
+def ReaderView(request: HttpRequest) -> HttpResponse:
+    result = Contact.objects.all()
+    values = Detail.objects.all()
+    return render(request, 'phone_book/reader.html', {'data': result, 'values':values})
 
-class ReaderView(ListView):
-    model = Contact
-    template_name = "phone_book/reader.html"
 
 
 class DeleterListView(ListView):
@@ -28,25 +30,34 @@ class DeleterView(DeleteView):
 
 class CreatorView(View):
     model = Contact
-    template_name = "phone_book/creator.html"
-    fields = ["contact_name", "phone_value"]
+    template_name = "phone_book/contact_form.html"
     success_url = reverse_lazy("contactcreator")
     initial = {"key": "value"}
-    form_class = CreateForm
+    form_class1 = CreateForm
 
     def get(self, request, *args, **kwargs):
-        form = self.form_class(initial=self.initial)
-        return render(request, self.template_name, {"form": form})
+        form1 = self.form_class1(initial=self.initial)
+        return render(request, self.template_name, {"form1": form1})
+
 
     def post(self, request):
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            contact_name = form.data.get("contact_name")
-            phone_value = form.data.get("phone_value")
-            creator_name = request.user
-            Contact.objects.create(contact_name=contact_name, phone_value=phone_value, creator_name=creator_name)
+        form1 = self.form_class1(request.POST, request.FILES, initial=self.initial)
+        if form1.is_valid():
+            contact = form1.save()
+            tags = querydict_to_dict(request.POST).get("tags")
+            for tag in tags:
+                contact.tags.add(tag)
 
-        return render(request, self.template_name, {"form": form})
+            details_form = querydict_to_dict(request.POST).get("details")
+
+            for detail_id in details_form:
+                detail = Detail.objects.get(id=detail_id)
+                Detail.objects.filter(id=detail_id).delete()
+                contact.detail.create(id=detail_id,contact_type=detail.contact_type, contact_value=detail.contact_value, contact=contact)
+
+            contact.creator=request.user
+            contact.save()
+        return render(request, self.template_name, {"form1": form1})
 
 
 class UpdaterListView(ListView):
@@ -57,5 +68,33 @@ class UpdaterListView(ListView):
 class UpdaterView(UpdateView):
     model = Contact
     template_name = "phone_book/updater_final.html"
-    fields = ["contact_name", "phone_value"]
+    # fields = ["contact_name"]
+    form_class = UpdateForm
     success_url = reverse_lazy("contactupdater_temp")
+
+
+class TagsCreatorView(CreateView):
+    model = Tag
+    fields = ['tag']
+    template_name = "phone_book/tags_creator.html"
+    success_url = reverse_lazy("contactcreator")
+
+
+class DetailsCreatorView(CreateView):
+    model = Detail
+    form_class = DetailForm
+    template_name = "phone_book/details_creator.html"
+    success_url = reverse_lazy("contactcreator")
+
+
+
+
+
+
+
+
+
+
+
+
+
